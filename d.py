c822c5888466b94953dfe533eed87d55cc6da064
kangming06 <74850890+kangming06@users.noreply.github.com>
2021-01-31 21:03:43 +0800

    Update d.py

--- d.py
+++ d.py
@@ -2,6 +2,8 @@ from imageai.Prediction import ImagePrediction
 import os
 import time
 import requests
+import pyttsx3
+
 start = time.time()
  
 #当前路径 包含需要预测的图片，模型文件
@@ -51,5 +53,70 @@ for eachPrediction, eachProbability in zip(predictions, probabilities):
 		outnumber+=1
 	print(outlist)
 
+
+print ("\ncost time:",end-start)
+from imageai.Prediction import ImagePrediction
+import os
+import time
+import requests
+start = time.time()
+ 
+#当前路径 包含需要预测的图片，模型文件
+execution_path = os.getcwd()
+ 
+#创建预测类
+prediction = ImagePrediction()
+ 
+#设置预测模型 有以下四种
+#SqueezeNet
+#prediction.setModelTypeAsSqueezeNet()
+#prediction.setModelPath(os.path.join(execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"))
+ 
+#ResNet50
+#prediction.setModelTypeAsInceptionV3()
+#prediction.setModelPath(os.path.join(execution_path, "inception_v3_weights_tf_dim_ordering_tf_kernels.h5"))
+ 
+#InceptionV3
+#prediction.setModelTypeAsResNet()
+#prediction.setModelPath(os.path.join(execution_path, "resnet50.h5"))
+ 
+#DenseNet121
+prediction.setModelTypeAsDenseNet()
+prediction.setModelPath(os.path.join(execution_path, "DenseNet-BC-121-32.h5"))
+ 
+prediction.loadModel()
+ 
+#预测图片，以及结果预测输出数目
+predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "1.jpg"), result_count=5 )
+ 
+#结束计时
+end = time.time()
+ 
+#输出结果
+#输出结果
+outlist={}
+outnumber=0
+zh_ch=0
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+	if eachProbability>=40:
+		eachPrediction=eachPrediction.replace("_"," ")
+		r = requests.get("http://fanyi.youdao.com/translate",params={'doctype': 'json','type': 'AUTO','i':eachPrediction})
+		result = r.json()
+		zh_ch = result['translateResult'][0][0]["tgt"]
+		print(zh_ch)
+		outlist[outnumber]=zh_ch
+		outnumber+=1
+	print(outlist)
+
+out=list(outlist)
+
+engine = pyttsx3.init()
+voice=engine.getProperty('voice')
+voices=engine.getProperty('voices')
+for item in voices:
+    print(item.id,item.languages)
+engine.setProperty('voice','zh')
+engine.say(out)
+engine.runAndWait()
 
 print ("\ncost time:",end-start)
